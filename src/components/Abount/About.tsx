95d8cbc71c2e72b80c2c3d074b8115733c6b18dd
Matheus Justino <matheus.justino@ccc.ufcg.edu.br>
2022-05-22 14:05:42 -0300

    removendo componente nao utilizado

--- src/components/Abount/About.tsx
+++ src/components/Abount/About.tsx
@@ -2,8 +2,6 @@
 import Image from "next/image";
 import React from "react";
 
-import { CodingSvg } from "./coding-svg";
-
 const About = () => {
   const src =
     "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80";
