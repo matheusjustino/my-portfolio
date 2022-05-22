a4ea2607c8cf4e2d5184eddb42ac6dcf2dde9420
Matheus Justino <matheus.justino@ccc.ufcg.edu.br>
2022-05-22 17:25:18 -0300

    env vars

--- src/pages/api/mailer.ts
+++ src/pages/api/mailer.ts
@@ -4,19 +4,19 @@ export default async function handler(req: any, res: any) {
   const { name, phone, email, subject, message } = req.body;
 
   const transporter = nodemailer.createTransport({
-    port: Number(process.env.REACT_APP_SMTP_PORT),
-    host: process.env.REACT_APP_SMTP_HOST,
+    port: Number(process.env.NEXT_PUBLIC_SMTP_PORT),
+    host: process.env.NEXT_PUBLIC_SMTP_HOST,
     auth: {
-      user: process.env.REACT_APP_EMAIL_FROM,
-      pass: process.env.REACT_APP_EMAIL_PASS,
+      user: process.env.NEXT_PUBLIC_EMAIL_FROM,
+      pass: process.env.NEXT_PUBLIC_EMAIL_PASS,
     },
     secure: true,
   });
 
   transporter.sendMail(
     {
-      from: process.env.REACT_APP_EMAIL_FROM,
-      to: process.env.REACT_APP_EMAIL_TO,
+      from: process.env.NEXT_PUBLIC_EMAIL_FROM,
+      to: process.env.NEXT_PUBLIC_EMAIL_TO,
       subject,
       text: `Nova mensagem de ${name},
 Telefone para contato: ${phone}
